9bb6411236f97746f65b14a6e70b6ca2d9db28a0
AlvaradoM <mauriciomj11@gmail.com>
2020-03-19 00:50:38 -0700

    test passed

--- anagrams.py
+++ anagrams.py
@@ -1,8 +1,6 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Command line utility that accepts a word file and prints a dictionary of
-anagrams for that file.
+"""Command line utility that accepts a word file and prints a dictionary of anagrams for that file.
 
 Module provides a function find_anagrams which can be used to do the same
 for an arbitrary list of strings.
@@ -12,41 +10,36 @@ import sys
 
 # Your name here, and any other people/sources who helped.
 # Give credit where credit is due.
-__author__ = "???"
+__author__ = "Mauricio"
 
 
 def alphabetize(string):
-    """Returns alphabetized version of the string"""
-    return "".join(sorted(string.lower()))
+	return "".join(sorted(string.lower()))
 
 
 def find_anagrams(words):
-    """
+	"""
     Returns a dictionary with keys that are alphabetized words and values
     that are all words that, when alphabetized, match the key.
     Example:
     {'dgo': ['dog'], 'act': ['cat', 'act']}
     """
-    anagrams = {
-        alphabetize(word): [
-            w for w in words
-            if alphabetize(w) == alphabetize(word)]
-        for word in words}
-    return anagrams
-
-
-def main(args):
-    # run find_anagrams() on first argument filename
-    if len(args) < 1:
-        print("Please specify a word file!")
-        sys.exit(1)
-
-    with open(args[0]) as f:
-        words = f.read().split()
-    anagram_dict = find_anagrams(words)
-    for k, v in anagram_dict.items():
-        print("{} : {}".format(k, v))
+	anagrams = {}
+	for word in words:
+		alphabetized_word = alphabetize(word)
+		if alphabetized_word not in anagrams:
+			anagrams[alphabetized_word] = [word]
+		else:
+			anagrams[alphabetized_word].append(word)
+	return anagrams
 
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+	# run find anagrams of first argument
+	if len(sys.argv) < 2:
+		print("Please specify a word file!")
+		sys.exit(1)
+	else:
+		with open(sys.argv[1], 'r') as handle:
+			words = handle.read().split()
+			print(find_anagrams(words))
